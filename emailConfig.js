8d3014057a2cee158d3b6a72b1434c943a89044e
Zoltán Éles <zoli1942@gmail.com>
2019-07-14 19:29:45 +0200

    Raise package version for security reason, add emailconfig to gitignore

--- emailConfig.js
+++
@@ -1,8 +0,0 @@
-module.exports=emailConfig={
-    userEmail:'yourmail@provider.com',
-    password:"pw1",
-    service: "gmail",
-    emailFrom:'Yourmail@prov.com',
-    emailTo:'Yourmail@prov.com',
-    subject: 'Subject'
-}
